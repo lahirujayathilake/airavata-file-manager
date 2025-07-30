69dd847e2e42f4a751a66a97f85a10d557f3f93f
lahiruj <lahirujayathilake@gmail.com>
2025-07-30 12:58:58 -0400

    updated the auth url to align with keycloak 24+

--- src/main/java/org/apache/airavata/filemgr/AuthenticationMgr.java
+++ src/main/java/org/apache/airavata/filemgr/AuthenticationMgr.java
@@ -40,7 +40,7 @@ public class AuthenticationMgr {
     public boolean authenticate(String username,String accessToken) throws AuthenticationException {
         try {
             if(accessToken != null && !accessToken.isEmpty()){
-                OAuthClientRequest request = new OAuthBearerClientRequest(hostName + "/auth/realms/" + realm + "/protocol/openid-connect/userinfo").
+                OAuthClientRequest request = new OAuthBearerClientRequest(hostName + "/realms/" + realm + "/protocol/openid-connect/userinfo").
                         buildQueryMessage();
                 URLConnectionClient ucc = new URLConnectionClient();
                 request.setHeader("Authorization","Bearer "+accessToken);
